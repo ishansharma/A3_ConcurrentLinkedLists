6066831fda367f50e9131896889538dd8669572f
Ishan <contact@ishansharma.com>
2018-11-25 22:36:38 -0600

    Adding first iteration of replace method

--- src/cll/ConcurrentList.java
+++ src/cll/ConcurrentList.java
@@ -140,6 +140,7 @@ public class ConcurrentList {
                         } else {
                             curr.marked = true;
                             pred.next = curr.next;
+                            size--;
                             return true;
                         }
                     }
@@ -148,6 +149,106 @@ public class ConcurrentList {
                 }
             } finally {
                 pred.l.unlock();
+            }
+        }
+    }
+
+    /**
+     * Given two keys, replace old with new
+     *
+     * @param oldItem Old item
+     * @param newItem New Item
+     * @return True if old item was removed or new item was added. False otherwise
+     */
+    public boolean replace(Integer oldItem, Integer newItem) {
+        int k_new = newItem.hashCode();
+        int k_old = oldItem.hashCode();
+        while (true) {
+            Node old_pred = head, old_curr = head.next, new_pred = head, new_curr = head.next;
+            // if old and new are same, we need to find where k_old is/should be
+            // if it's there, don't do anything.
+            // if it's not there, insert k_new
+            if (k_old == k_new) {
+                while (old_curr.key < k_old) {
+                    old_pred = old_curr;
+                    old_curr = old_curr.next;
+                }
+                old_pred.l.lock();
+                try {
+                    old_curr.l.lock();
+                    try {
+                        if (validate(old_pred, old_curr)) {
+                            // if the key is already there, we don't need to do anything
+                            if (old_curr.key == k_old) {
+                                return false;
+                            } else {
+                                Node node = new Node(newItem);
+                                node.next = old_curr;
+                                old_pred.next = node;
+                                size++;
+                                return true;
+                            }
+                        }
+                    } finally {
+                        old_curr.l.unlock();
+                    }
+                } finally {
+                    old_pred.l.unlock();
+                }
+            } else {
+                boolean oldDeleted, newAdded;
+                // in case old and new aren't same, find where k_old is/should be
+                // find where k_new is/should be and act accordingly
+                while (old_curr.key < k_old || new_curr.key < k_new) {
+                    // update only if we haven't found a position for old_curr
+                    if (old_curr.key < k_old) {
+                        old_pred = old_curr;
+                        old_curr = old_curr.next;
+                    }
+
+                    // update only if we haven't found new_curr
+                    if (new_curr.key < k_new) {
+                        new_pred = new_curr;
+                        new_curr = new_curr.next;
+                    }
+                }
+
+                old_pred.l.lock();
+                old_curr.l.lock();
+                new_pred.l.lock();
+                new_curr.l.lock();
+                try {
+                    if (validate(old_pred, old_curr) && validate(new_pred, new_curr)) {
+                        // add new key if it's not present
+                        if (new_curr.key == k_new) {
+                            newAdded = false;
+                        } else {
+                            Node node = new Node(newItem);
+                            node.next = new_curr;
+                            new_pred.next = node;
+                            size++;
+                            newAdded = true;
+                        }
+
+                        // remove old key if it's present
+                        if (old_curr.key != k_old) {
+                            oldDeleted = false;
+                        } else {
+                            old_curr.marked = true;
+                            old_pred.next = old_curr.next;
+                            size--;
+                            oldDeleted = true;
+                        }
+
+                        // if we did any of the two operations successfully, return true
+                        return oldDeleted || newAdded;
+                    }
+                } finally {
+                    old_pred.l.unlock();
+                    old_curr.l.unlock();
+                    new_pred.l.unlock();
+                    new_curr.l.unlock();
+                }
             }
         }
     }
--- tests/cll/ConcurrentListTest.java
+++ tests/cll/ConcurrentListTest.java
@@ -51,4 +51,32 @@ class ConcurrentListTest {
         l.insert(1);
         assertEquals("1", l.toString());
     }
+
+    @Test
+    void replaceWithSame() {
+        ConcurrentList l = new ConcurrentList();
+        l.insert(1);
+        l.insert(4);
+        l.insert(6);
+        assertTrue(l.replace(2, 2));
+        assertEquals("1 2 4 6", l.toString());
+
+        assertFalse(l.replace(4, 4));
+        assertEquals("1 2 4 6", l.toString());
+    }
+
+    @Test
+    void replaceWithDifferent() {
+        ConcurrentList l = new ConcurrentList();
+        l.insert(1);
+        l.insert(2);
+        l.insert(5);
+        l.insert(10);
+
+        assertTrue(l.replace(2, 7));
+        assertEquals("1 5 7 10", l.toString());
+
+        assertTrue(l.replace(7, 8));
+        assertEquals("1 5 8 10", l.toString());
+    }
 }
